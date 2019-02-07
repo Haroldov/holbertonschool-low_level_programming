629f578a1448e56ce6b4e388479386fc116e709c
Haroldo <hvelez020@gmail.com>
2019-02-07 20:52:24 +0000

    uploading point 2 incomplete

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - the program checks the sign of a number
+ *
+ *Return: returns an int zero
+ */
+
+int main(void)
+{
+	putchar("{a..z}");
+	return (0);
+}
