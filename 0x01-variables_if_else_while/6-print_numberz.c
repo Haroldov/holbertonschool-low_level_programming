da11b6917a18b63b685306a8c16b203e30455803
Haroldo Vélez <hvelez020@gmail.com>
2019-02-08 00:24:43 +0000

    uploading task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - the program prints 0..10
+ *
+ *Return: returns an int zero
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
