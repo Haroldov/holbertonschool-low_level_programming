e0c6f7941444ca25f835333ca329d18d7d7b2440
Haroldo <hvelez020@gmail.com>
2019-02-12 23:47:43 +0000

    uploading task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+ *add - sums two numbers
+ *@a: first number to be summed
+ *@b: second number to be summed
+ *Return: return the sum of two numbers
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
