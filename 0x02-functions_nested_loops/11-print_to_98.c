f5ae045876383144d54c5140f86356808498a02f
Haroldo <hvelez020@gmail.com>
2019-02-13 13:13:03 +0000

    the last checker

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,17 +8,26 @@
 
 void print_to_98(int n)
 {
+	int init;
+
+	init = n;
 	while (n <= 98)
 	{
 		if (n != 98)
 		{
 			printf("%i, ", n);
-			n++;
+			if (init > 98)
+				n--;
+			else
+				n++;
 		}
 		else
 		{
 			printf("%i\n", n);
-			n++;
+			if (init > 98)
+				n--;
+			else
+				n++;
 		}
 	}
 }
