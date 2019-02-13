b4a48a2fdf62e809e1f1101769bd45cd2238454c
Haroldo <hvelez020@gmail.com>
2019-02-13 13:08:09 +0000

    adding doc to last task

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+
+/**
+ *print_to_98 - print the n integers numbers
+ *@n: n number to be printed
+ *Return: none
+ */
+
 void print_to_98(int n)
 {
 	while (n <= 98)
@@ -14,5 +21,4 @@ void print_to_98(int n)
 			n++;
 		}
 	}
-	return;
 }
