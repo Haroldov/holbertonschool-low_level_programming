125c64838104614df9049f7411d74d39fa4d4afb
Haroldo <hvelez020@gmail.com>
2019-02-13 13:19:15 +0000

    printing also for the number above 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,23 +11,13 @@ void print_to_98(int n)
 	int init;
 
 	init = n;
-	while (n <= 98)
+	while (n != 98)
 	{
-		if (n != 98)
-		{
-			printf("%i, ", n);
-			if (init > 98)
-				n--;
-			else
-				n++;
-		}
+		printf("%i, ", n);
+		if (init > 98)
+			n--;
 		else
-		{
-			printf("%i\n", n);
-			if (init > 98)
-				n--;
-			else
-				n++;
-		}
+			n++;
 	}
+	printf("%i\n", n);
 }
