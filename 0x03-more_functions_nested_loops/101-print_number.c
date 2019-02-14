7d9516cf98452b08f728a05af9a80edad6959885
Haroldo <hvelez020@gmail.com>
2019-02-14 21:17:15 +0000

    adding documentation

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -1,11 +1,17 @@
 #include "holberton.h"
 
+/**
+ *print_number - prints a number
+ *@n: the number to be printed
+ *Return: none
+ */
+
 void print_number(int n)
 {
 	int dec, tmp, sw, swn;
 
 	swn = 0;
-	if (n < 10)
+	if (n < 0)
 	{
 		n *= -1;
 		swn = 1;
@@ -20,7 +26,7 @@ void print_number(int n)
 		sw = 0;
 		for (dec = 1e9; dec != 0; dec /= 10)
 		{
-			if (((tmp/dec)) || (sw == 1))
+			if (((tmp / dec)) || (sw == 1))
 			{
 				if (swn)
 				{
