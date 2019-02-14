bcb1235306ee70af15342705059276d0653141c6
Haroldo <hvelez020@gmail.com>
2019-02-14 19:32:02 +0000

    adding task 10

---
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ *print_triangle - prints a triangle
+ *@size:  size of the triangle
+ *Return: none
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = size; i != 0; i--)
+                {
+                        for (j = 0; j <= size; j++)
+			{
+				if (j >= i)
+					_putchar('#');
+				else
+					_putchar(' ');
+			}
+                        _putchar('\n');
+                }
+	}
+}
