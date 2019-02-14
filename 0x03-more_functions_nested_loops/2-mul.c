789f45364851287ca6ae396419bb612c624bc1b9
Haroldo <hvelez020@gmail.com>
2019-02-14 14:34:39 +0000

    adding task 1 and 2

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ *mul - multiplies two integers.
+ *@a: first integer to be multiplied.
+ *@b: second integer to be multiplied.
+ *Return: the multiplication of a and b.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
