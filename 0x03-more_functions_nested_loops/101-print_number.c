0bc666bb9da1c2a0044af16451098b51e738e29b
Haroldo <hvelez020@gmail.com>
2019-02-14 21:14:00 +0000

    expanding to negative numbers

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -2,11 +2,13 @@
 
 void print_number(int n)
 {
-	int dec, tmp, sw;
+	int dec, tmp, sw, swn;
 
+	swn = 0;
 	if (n < 10)
 	{
 		n *= -1;
+		swn = 1;
 	}
 	if (n % 10 == 0)
 	{
@@ -20,6 +22,11 @@ void print_number(int n)
 		{
 			if (((tmp/dec)) || (sw == 1))
 			{
+				if (swn)
+				{
+					_putchar('-');
+					swn = 0;
+				}
 				_putchar((tmp / dec)  + '0');
 				tmp = (n % dec);
 				sw = 1;
