83c6ae6a427ad53e69d5f5c0afd5259502f2fb38
Haroldo <hvelez020@gmail.com>
2019-02-14 19:34:31 +0000

    removing extra space at the beginning of the triangle

--- 0x03-more_functions_nested_loops/10-print_triangle.c
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -18,7 +18,7 @@ void print_triangle(int size)
 	{
 		for (i = size; i != 0; i--)
                 {
-                        for (j = 0; j <= size; j++)
+                        for (j = 1; j <= size; j++)
 			{
 				if (j >= i)
 					_putchar('#');
