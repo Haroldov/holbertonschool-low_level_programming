81f20aa1f8ce3e764482b8687b000cf9dc06407e
Haroldo <hvelez020@gmail.com>
2019-02-19 20:40:00 +0000

    adding white space

--- 0x04-pointers_arrays_strings/8-main.c
+++ 0x04-pointers_arrays_strings/8-main.c
@@ -7,13 +7,15 @@
  */
 int main(void)
 {
-	int array[5];
+	int array[7];
 
 	array[0] = 98;
 	array[1] = 402;
 	array[2] = -198;
 	array[3] = 298;
 	array[4] = -1024;
+	array[5] = 234324;
+	array[6] = 234234;
 	print_array(array, 5);
 	return (0);
 }
