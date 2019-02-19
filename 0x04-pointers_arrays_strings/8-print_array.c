90e9b5193e9d7084a7d3f2574f19a065a61bcda8
Haroldo <hvelez020@gmail.com>
2019-02-19 20:25:06 +0000

    adding some changes

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -12,6 +12,6 @@ void print_array(int *a, int n)
 	int i;
 
 	for (i = 0; i < (n - 1); i++)
-		printf("%i, ", *(a + i));
-	printf("%i\n", *(a + i));
+		printf("%i, ", a[i]);
+	printf("%i\n", a[i]);
 }
