b50319b23cf7c2daf07a46ff707fcdab541af0b3
Haroldo <hvelez020@gmail.com>
2019-02-19 19:45:50 +0000

    adding task number 5

---
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ *rev_string -  reverses a string.
+ *@s: pointer to the string that will be reversed.
+ *Return: none
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int j;
+	char tmp;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+	for (j = i; j >= 0; j--)
+	{
+		if (j <= i/2)
+		{
+			tmp = *(s + j);
+			*(s + j) = *(s + (i - j));
+			*(s + (i - j)) = tmp;
+		}
+	}
+}
