04e75ff079949190927d31ba8ad676b0e5f07f5d
Haroldo <hvelez020@gmail.com>
2019-02-19 15:50:16 +0000

    adding readme file and task number 0

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ *reset_to_98 - takes a pointer to an int as parameter and updates it to 98.
+ *@n: pointer to the number to be updated
+ *Return: none
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
