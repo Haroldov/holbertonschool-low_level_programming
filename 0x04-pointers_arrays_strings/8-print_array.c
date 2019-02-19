8f350b85cb83c48393e92326825b7bec777c8da1
Haroldo <hvelez020@gmail.com>
2019-02-19 20:12:07 +0000

    adding task number 8

---
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *print_array - prints N elements of an array of int,followed by a new line
+ *@a: pointer of the array where the numbers are
+ *@n: number of elements of the array to be printed
+ *Return: none
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+		printf("%i, ", *(a + i));
+	printf("%i\n", *(a + i));
+}
