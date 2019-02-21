3f03d26a2f71dc9c42fb98ea4cce6a6cde050058
Haroldo <hvelez020@gmail.com>
2019-02-21 22:54:58 +0000

    fixing initilization of a variable

--- 0x05-pointers_arrays_strings/5-main.c
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdio.h>
+#include <ctype.h>
 
 /**
  * main - check the code for Holberton School students.
@@ -8,9 +9,10 @@
  */
 int main(void)
 {
-	char s[] = "abcdefghijklmnopqtrxyz!\n";
+	char s[] = "a b c d e f g h i j k l m n o p q r s t u v w x y z!\n";
 	char *p;
 
+	printf("%p", s);
 	p = string_toupper(s);
 	printf("%s", p);
 	printf("%s", s);
--- 0x05-pointers_arrays_strings/5-string_toupper.c
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -8,13 +8,14 @@
 
 char *string_toupper(char *s)
 {
-	int i;
+	int i = 0;
 
 	while (*(s + i))
 	{
 		if ((*(s + i) >= 'a') && (*(s + i) <= 'z'))
 			*(s + i) -= 32;
 		i++;
+
 	}
 	return (s);
 }
