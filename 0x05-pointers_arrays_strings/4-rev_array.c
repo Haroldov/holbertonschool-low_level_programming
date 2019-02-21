84e40dbe0b3aa4e4757eb9c8941c52ddb0be6c23
Haroldo <hvelez020@gmail.com>
2019-02-21 21:35:12 +0000

    adding task number 4

---
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ *rev_array -  reverses a array.
+ *@a: pointer to the array that will be reversed.
+ *@n: number of elements of the array.
+ *Return: none
+ */
+
+void reverse_array(int *a, int n)
+{
+	int j;
+	char tmp;
+
+	n--;
+	for (j = n; j >= 0; j--)
+	{
+		if (j <= (n / 2))
+		{
+			tmp = *(a + j);
+			*(a + j) = *(a + (n - j));
+			*(a + (n - j)) = tmp;
+		}
+	}
+}
