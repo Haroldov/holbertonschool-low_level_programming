266700f53e579045d616d91c50ead9500fbc4692
Haroldo <hvelez020@gmail.com>
2019-02-21 20:01:30 +0000

    adding task number 2

---
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *_strncpy - copies a string.
+ *@dest: string destination
+ *@src: string to be copied to dest.
+ *@n: it will use at most n bytes from src.
+ *Return: pointer to the resulting string.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (*(src + i) && (i <= (n - 1)))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
