70e35b59955d2c43bc742c38b39f85fd38d8133f
Haroldo Vélez <hvelez020@gmail.com>
2019-02-23 16:00:02 +0000

    adding advanced task (from string to int)

---
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+
+/**
+ *_atoi -  convert a string to an integer
+ *@s: string to be converted
+ *Return: the resulted integer.
+ */
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int sign = 1;
+	int num = 0;
+	int cont = 0;
+	int tmp = 1e9;
+
+	while (*(s + i))
+	{
+		if (*(s + i) == '-')
+			sign *= -1;
+		if ((*(s + i) >= '0') && (*(s + i) <= '9'))
+		{
+			if (cont == 0)
+			{
+				cont = i;
+				while (((*(s + cont) >= '0') && (*(s + cont) <= '9')))
+				{
+					cont++;
+				}
+				cont -= i;
+				while (cont != 10)
+				{
+					tmp /= 10;
+					cont++;
+				}
+			}
+			num += ((*(s + i)) - '0') * tmp;
+			tmp /= 10;
+			if (tmp == 0)
+				break;
+		}
+		i++;
+	}
+	return (sign * num);
+}
--- 0x04-pointers_arrays_strings/holberton.h
+++ 0x04-pointers_arrays_strings/holberton.h
@@ -9,3 +9,4 @@ void swap_int(int *a, int *b);
 int _putchar(char c);
 void reset_to_98(int *n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
