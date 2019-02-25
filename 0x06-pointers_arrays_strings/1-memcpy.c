20e5bbb86886b88e0c08089cf6fdebed5507e4a7
Haroldo <hvelez020@gmail.com>
2019-02-25 18:47:51 +0000

    adding task number 2

---
+++ 0x06-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+/**
+ *_memcpy - copies memory area.
+ *@dest: destination pointer
+ *@src: source pointer
+ *@n: number of bytes to be copied
+ *Return: a pointer to the memory area dest.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	for (; n; n--)
+		*(dest + n) = *(src + n);
+	return (dest);
+}
