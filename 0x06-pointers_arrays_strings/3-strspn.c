e3f05f4511d69976395da029a7af5e19c88ac1b9
Haroldo <hvelez020@gmail.com>
2019-02-25 20:40:28 +0000

    correcting task 3, initial segment is until no more c are found in s

--- 0x06-pointers_arrays_strings/3-strspn.c
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -10,16 +10,20 @@ unsigned int _strspn(char *s, char *accept)
 	int i = 0;
 	int j = 0;
 	unsigned int cont = 0;
+	unsigned int tmp = 0;
 
-	while ((*(s + i) != ' ') ||(*(s + i) != ',') ||(*(s + i) != '.'))
+	while (*(s + i))
 	{
 		j = 0;
+		tmp = cont;
 		while (*(accept + j))
 		{
 			if (*(s + i) == *(accept + j))
 				cont++;
 			j++;
 		}
+		if (tmp == cont)
+			break;
 		i++;
 	}
 	return (cont);
