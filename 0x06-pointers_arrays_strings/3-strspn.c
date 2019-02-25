a6e097b3638bbe4b877421976c248d26bbbadb26
Haroldo <hvelez020@gmail.com>
2019-02-25 20:09:17 +0000

    adding first trial of task number 3

---
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+/**
+ *_strspn - gets the length of a prefix substring.
+ *@s: string
+ *@accept: characters to be accepted in the string
+ *Return: number of bytes in the initial segment
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0;
+        int j = 0;
+        unsigned int cont = 0;
+
+	while (*(s + i) != ',')
+	{
+		j = 0;
+		while (*(accept + j))
+		{
+			if (*(s + i) == *(accept + j))
+				cont++;
+			j++;
+		}
+		i++;
+	}
+	return (cont);
+}
