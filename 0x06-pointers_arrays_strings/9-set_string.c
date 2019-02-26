68668a780c82ebbf19b543a8337bcae82e04eac2
Haroldo <hvelez020@gmail.com>
2019-02-26 01:16:10 +0000

    adding task 9

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *set_string - sets the value of a pointer to a char.
+ *@s: double pointer to a char
+ *@to: destination
+ *Return: none
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
