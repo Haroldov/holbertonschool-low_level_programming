f5fb14eaafdae056dbaac4f51b9ceef74132cb8f
Haroldo <hvelez020@gmail.com>
2019-03-01 18:29:47 +0000

    ss

--- 0x07-recursion/100-wildcmp.c
+++ 0x07-recursion/100-wildcmp.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 /**
  *wildcmp - compares two strings
  *@s1: pointer to the string 1
@@ -6,9 +5,12 @@
  *Return: 1 if they are identical 0 otherwise
  */
 
+int get_as(char *s, int i);
+
 int wildcmp(char *s1, char *s2)
 {
-	printf("%s ------- %s\n", s1, s2);
+	int len = 0;
+
 	if (*s1 == '\0')
 	{
 		if (*s2 == '\0')
@@ -24,18 +26,20 @@ int wildcmp(char *s1, char *s2)
 		}
 		else
 		{
+			len = get_as(s1, 0);
 			if (*s1 == '*')
 			{
-				if (*(s1 + 1) == *s2)
+				if (*(s1 + len) == *s2)
 					return (wildcmp(s1 += 2, s2 += 1));
 				else
 					return (wildcmp(s1, ++s2));
 			}
 			else
 			{
+				len = get_as(s2, 0);
 				if (*s2 == '*')
 				{
-					if (*(s2 + 1) == *s1)
+					if (*(s2 + len) == *s1)
 						return (wildcmp(s1 += 1, s2 += 2));
 					else
 						return (wildcmp(++s1, s2));
@@ -47,4 +51,12 @@ int wildcmp(char *s1, char *s2)
 			}
 		}
 	}
+}
+
+int get_as(char *s, int i)
+{
+	if (i != '*')
+		return (i);
+	else
+		return (get_as(s, ++i));
 }
