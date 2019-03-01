e94e23d071de2c1ac5c2eb5777415f45ca00305a
Haroldo <hvelez020@gmail.com>
2019-03-01 21:19:16 +0000

    adding README file and task number 0

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - prints its name, followed by a new line
+ *@argc: number of arguments
+ *@argv: pointer to arguments
+ *Return: 1 if successful 0 otherwise
+ */
+
+int main(int argc, char *argv[])
+{
+	if (*argv == NULL)
+		return (1);
+	else
+		printf("%s\n", *argv);
+	return (main(argc, argv + 1));
+}
