a6c0897b11ee830e545ad98980be23c30754ac04
Haroldo <hvelez020@gmail.com>
2019-03-01 15:58:05 +0000

    adding task number 7

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -24,6 +24,13 @@ int is_palindrome(char *s)
 	}
 }
 
+/**
+ *get_length - auxiliar function to get the length of a string recursively
+ *@s: pointer to string
+ *@i: iterator
+ *Return: the lenght of a string
+ */
+
 int get_length(char *s, int i)
 {
 	if (*(s + i) == '\0')
@@ -32,11 +39,19 @@ int get_length(char *s, int i)
 		return (get_length(s, ++i));
 }
 
+/**
+ *compare_s - check if a string is palindrome
+ *@s: string to be checked
+ *@i: iterator
+ *@len: length of the string
+ *Return: 1 if palindrome 0 otherwise.
+ */
+
 int compare_s(char *s, int i, int len)
 {
 	if (*(s + i) == *(s + (len - i)))
 	{
-		if (i == len/2)
+		if (i == len / 2)
 			return (1);
 		return (compare_s(s, --i, len));
 	}
