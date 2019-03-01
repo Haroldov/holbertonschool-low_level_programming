52c080b8792480dbae5e0103e7ab0111f89a3cdb
Haroldo <hvelez020@gmail.com>
2019-03-01 21:52:36 +0000

    adding task number 3

---
+++ 0x09-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - multiplies two numbers.
+ *@argc: number of arguments
+ *@argv: pointer to arguments
+ *Return: 1 if error, otherwise the multiplication
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%i\n", atoi(*(argv + 2)) * atoi(*(argv + 1)));
+		return (atoi(*(argv + 2)) * atoi(*(argv + 1)));
+	}
+}
