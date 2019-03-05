f41ad54a22b62861da1a683cbe445fc319a53cdd
Javier F. Arias <752@holbertonschool.com>
2019-03-05 21:10:04 +0000

    adding brackets that were missing

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -17,10 +17,11 @@ char *create_array(unsigned int size, char c)
 
 	arr = malloc(size * sizeof(char));
 	if (arr == NULL)
+	{
 		free(arr);
 		return (NULL);
+	}
 	for (i = 0; i < (size * sizeof(char)); i++)
 		*(arr + i) = c;
-
 	return (arr);
 }
