5e2ea50047c8b0d86119d56b0c8c338231d52789
Javier F. Arias <752@holbertonschool.com>
2019-03-05 21:22:48 +0000

    adding task number 1

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ *_strdup - copy a string
+ *@str: string to be copyied
+ *Return: pointer to a newly allocated space in memory
+ */
+
+char *_strdup(char *str)
+{
+	int size = 0;
+	char *cpystr;
+
+	while (*(str + size) != '\0')
+		size++;
+
+	cpystr = malloc(size * sizeof(char));
+	if (cpystr == NULL || str == NULL)
+	{
+		free(cpystr);
+		return (NULL);
+	}
+	else
+	{
+		for (; size != -1; size--)
+			*(cpystr + size) = *(str + size);
+		return (cpystr);
+	}
+}
