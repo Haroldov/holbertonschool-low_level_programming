8e0493c1e91e7d5080bfb4aaa2e06ce9e6bdb9a1
Javier F. Arias <752@holbertonschool.com>
2019-03-05 21:01:28 +0000

    releasing block of memory before returning

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -17,8 +17,10 @@ char *create_array(unsigned int size, char c)
 
 	arr = malloc(size * sizeof(char));
 	if (arr == NULL)
+		free(arr);
 		return (NULL);
 	for (i = 0; i < (size * sizeof(char)); i++)
 		*(arr + i) = c;
+	free(arr);
 	return (arr);
 }
