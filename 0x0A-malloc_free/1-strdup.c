5581322546f33c643147c5daa179cbfb8ae1f75a
Javier F. Arias <752@holbertonschool.com>
2019-03-05 21:33:20 +0000

    adding condition if input is a null pointer

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -12,19 +12,22 @@ char *_strdup(char *str)
 {
 	int size = 0;
 	char *cpystr;
-
+	if ( str == NULL)
+	{
+		return (NULL);
+	}
 	while (*(str + size) != '\0')
 		size++;
 
 	cpystr = malloc(size * sizeof(char));
-	if (cpystr == NULL || str == NULL)
+	if (cpystr == NULL)
 	{
 		free(cpystr);
 		return (NULL);
 	}
 	else
 	{
-		for (; size != -1; size--)
+		for (; size >= 0; size--)
 			*(cpystr + size) = *(str + size);
 		return (cpystr);
 	}
