c9fc0ebe481bef324577fb88eb71e3b6f274fc16
Javier F. Arias <752@holbertonschool.com>
2019-03-05 23:48:49 +0000

    adding task number 3

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ *alloc_grid - creates a 2 dimensional array of integers
+ *@width: width of the array
+ *@height: height of the array
+ *Return: a pointer to a 2 dimensional array of integers
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int *tmp;
+	int **arr;
+	int i;
+	int size = width * height;
+
+	if (width == 0 || height == 0)
+		return (NULL);
+
+	tmp = (int *) malloc(size * sizeof(int));
+	arr = (int **) malloc(height * sizeof(int *));
+
+	if (arr == NULL || tmp == NULL)
+	{
+		free(arr);
+		free(tmp);
+		return (NULL);
+	}
+
+
+	for (i = 0; i < height; i++)
+	{
+		tmp += i * width;
+		arr[i] = tmp;
+		tmp = arr[0];
+	}
+	tmp = arr[0];
+
+	for (i = 0; i < size; i++)
+		tmp[i] = 0;
+	return (arr);
+}
