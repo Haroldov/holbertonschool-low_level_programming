f23501bc05ef6a562308d8a2d26dd1c1f79b7827
Javier F. Arias <752@holbertonschool.com>
2019-03-05 21:37:50 +0000

    putting betty style

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -12,23 +12,23 @@ char *_strdup(char *str)
 {
 	int size = 0;
 	char *cpystr;
-	if ( str == NULL)
+
+	if (str == NULL)
 	{
 		return (NULL);
 	}
+
 	while (*(str + size) != '\0')
 		size++;
 
 	cpystr = malloc(size * sizeof(char));
+
 	if (cpystr == NULL)
 	{
 		free(cpystr);
 		return (NULL);
 	}
-	else
-	{
-		for (; size >= 0; size--)
-			*(cpystr + size) = *(str + size);
-		return (cpystr);
-	}
+	for (; size >= 0; size--)
+		*(cpystr + size) = *(str + size);
+	return (cpystr);
 }
