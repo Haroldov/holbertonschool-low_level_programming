ca0b5ecc13e4dc43b0f54ed8b2ebea3e199d3da1
Javier F. Arias <752@holbertonschool.com>
2019-03-06 00:40:44 +0000

    adding releasing

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -24,10 +24,6 @@ int **alloc_grid(int width, int height)
 
 	if (arr == NULL || tmp == NULL)
 	{
-		for (i = 0; i < height; i++)
-		{
-			free(arr[i]);
-		}
 		free(arr);
 		free(tmp);
 		return (NULL);
