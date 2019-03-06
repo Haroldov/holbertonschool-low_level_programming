2f3f3f48870aa4db17a9fde2ca6eac4a7af50c68
Haroldo Vélez <hvelez020@gmail.com>
2019-03-06 04:14:41 +0000

    adding last format

---
+++ 0x0A-malloc_free/100-strtow.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ *strtow -  splits a string into words
+ *@str: string to be splitted
+ *Return: pointer to string
+ */
+
+char **strtow(char *str)
+{
+	return (str);
+}
