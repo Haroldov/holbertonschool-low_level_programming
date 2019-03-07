17f1136b38471ae438ce37efca09b33a9e076752
Javier F. Arias <752@holbertonschool.com>
2019-03-07 17:50:16 +0000

    adding task

--- 0x0B-more_malloc_free/1-main.c
+++ 0x0B-more_malloc_free/1-main.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	char *concat;
 
-	concat = string_nconcat("Holberton ", "skjfhjakh", 5);
+	concat = string_nconcat("Hello ", "hola", 250);
 	printf("%s\n", concat);
 	free(concat);
 	return (0);
