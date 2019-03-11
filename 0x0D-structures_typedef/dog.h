7ff031d15644a3193caefc48fd7d165a2f6b39ef
Haroldo Vélez <hvelez020@gmail.com>
2019-03-11 19:12:42 +0000

    adding README and first task

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG
+#define DOG
+/**
+ *struct dog - structure for a dog
+ *@name: name of the dog
+ *@age: age of the dog
+ *@owner: owner of the do
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
