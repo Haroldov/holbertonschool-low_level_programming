ca14176d79309ab628501acaff11356109d96d6d
Haroldo Vélez <hvelez020@gmail.com>
2019-03-13 15:20:04 +0000

    adding README file and first task

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ *print_name - prints a name
+ *@name: pointer to the name to be printed
+ *@f: pointer to the function that prints
+ *Return: none
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
---
+++ 0x0E-function_pointers/function_pointers.h
@@ -0,0 +1,5 @@
+#ifndef __FUNCTION_POINTERS__
+#define __FUNCTION_POINTERS__
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+#endif
