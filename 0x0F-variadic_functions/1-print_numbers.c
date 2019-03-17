343215e30e96dada6caa012e857ccf90551237b7
Haroldo Vélez <hvelez020@gmail.com>
2019-03-17 21:15:13 +0000

    back to a previous version of the code and fixing a mistake when n is zero

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -12,15 +12,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_list(valist);
-	va_start(valist, n);
-	if (n != 0 && separator != NULL)
+	if (n != 0)
 	{
+		va_start(valist, n);
 		for (i = 0; i < n - 1; i++)
 		{
-			/*if (separator != NULL)*/
+			if (separator != NULL)
 				printf("%i%s", va_arg(valist, int), separator);
-			/*else
-			  printf("%i", va_arg(valist, int));*/
+			else
+			  printf("%i", va_arg(valist, int));
 		}
 		printf("%i\n", va_arg(valist, int));
 	}
