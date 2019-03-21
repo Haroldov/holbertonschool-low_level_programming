9b926c136a5fa24d93013d4faf4d612dc1dab917
Haroldo Vélez <hvelez020@gmail.com>
2019-03-21 23:47:09 +0000

    fixing assignment between two different type of variables

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
 #include <string.h>
@@ -12,12 +13,18 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = NULL;
+
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 	(*new).str = strdup(str);
+	if ((*new).str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
 	(*new).len = strlen(str);
-	(*new).next = (*head)->next;
-	(*head)->next = &new;
-	return ((*head)->next);
+	(*new).next = *head;
+	*head = new;
+	return (new);
 }
