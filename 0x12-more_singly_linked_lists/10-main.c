e172082c8fce47e8ba02fb6b916accea764d8729
Haroldo Vélez <hvelez020@gmail.com>
2019-03-25 17:36:30 +0000

    any update

--- 0x12-more_singly_linked_lists/10-main.c
+++ 0x12-more_singly_linked_lists/10-main.c
@@ -23,7 +23,7 @@ int main(void)
 	add_nodeint_end(&head, 1024);
 	print_listint(head);
 	printf("-----------------\n");
-	delete_nodeint_at_index(&head, 5);
+	delete_nodeint_at_index(&head, 20);
 	print_listint(head);
 	printf("-----------------\n");
 	delete_nodeint_at_index(&head, 0);
@@ -62,7 +62,7 @@ int main(void)
 	printf("-----------------\n");
 	delete_nodeint_at_index(&head, 0);
 	printf("-----------------\n");
-	delete_nodeint_at_index(&head, 0);
+	delete_nodeint_at_index(&head, 10);
 	print_listint(head);
 	return (0);
 }
