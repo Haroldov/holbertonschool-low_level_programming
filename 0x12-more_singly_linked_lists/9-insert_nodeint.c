9d9177cff20d1655ef1c15b536dccf248524be79
Haroldo Vélez <hvelez020@gmail.com>
2019-03-26 12:12:47 +0000

    fixing if condition from = to ==

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new = malloc(sizeof(listint_t)), *tmp = *head;
 	unsigned int cont = 0;
 
-	if (new == NULL || head == NULL || *head = NULL)
+	if (new == NULL || head == NULL || *head == NULL)
 		return (NULL);
 	new->n = n;
 	while (cont != idx - 1 && tmp != NULL && idx != 0)
