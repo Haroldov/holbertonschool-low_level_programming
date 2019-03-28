86577d1ec4ab001a330ce3e66abfea49f11acce1
Haroldo Vélez <hvelez020@gmail.com>
2019-03-28 17:41:30 +0000

    Add: task #4

---
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *clear_bit - clear the value of a bit to 1 at a given index
+ *@n: pointer to the long int number (8 bytes)
+ *@index: index of the bit to be cleared
+ *Return: 1 if it worked -1 if an error ocurred.
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int tmp = ~(1 << index);
+
+	if (n == NULL || index > 32)
+		return (-1);
+	*n = *n & tmp;
+	return (1);
+}
--- 0x13-bit_manipulation/holberton.h
+++ 0x13-bit_manipulation/holberton.h
@@ -1,6 +1,7 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 #include <stdio.h>
+int clear_bit(unsigned long int *n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
 void print_binary(unsigned long int n);
