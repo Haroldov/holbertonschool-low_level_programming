ee4499dfa53df4bcc9f5dce201151edb307e5b98
Haroldo Vélez <hvelez020@gmail.com>
2019-03-28 17:38:00 +0000

    Fix: task #3 returning -1 when index is greater than 32

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -11,7 +11,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int tmp = (1 << index);
 
-	if (n == NULL)
+	if (n == NULL || index > 32)
 		return (-1);
 	*n = *n | tmp;
 	return (1);
