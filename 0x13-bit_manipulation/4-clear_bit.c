19397dd44efc592b702624e0c72b830bf221ac09
Haroldo Vélez <hvelez020@gmail.com>
2019-03-28 20:13:50 +0000

    Fix: tasks #2,3,4 multiplying the size in bytes by 8

--- 0x13-bit_manipulation/4-clear_bit.c
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -11,7 +11,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int tmp = ~(1 << index);
 
-	if (n == NULL || index > sizeof(unsigned long int))
+	if (n == NULL || index > sizeof(unsigned long int) * 8)
 		return (-1);
 	*n = *n & tmp;
 	return (1);
