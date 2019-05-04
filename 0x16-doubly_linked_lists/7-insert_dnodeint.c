b8dc2a8564259ffc474e062c01d143d9a6ab2aeb
Haroldo Vélez <hvelez020@gmail.com>
2019-05-04 20:05:48 +0000

    [fix] task #7

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,19 +16,29 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (new_node == NULL)
 		return (NULL);
+	if (idx == 0)
+	{
+		(*new_node).n = n;
+		(*new_node).next = *h;
+		(*new_node).prev = NULL;
+		*h = new_node;
+		return (new_node);
+	}
 	idx--;
 	while (tmp != NULL && idx != 0)
 	{
 		tmp = (*tmp).next;
 		idx--;
 	}
+	printf("%i\n", idx);
 	if (idx == 0)
 	{
 		(*new_node).n = n;
 		(*new_node).prev = tmp;
 		(*new_node).next = (*tmp).next;
 		(*tmp).next = new_node;
-		(*new_node->next).prev = new_node;
+		if (new_node->next != NULL)
+			(*(new_node->next)).prev = new_node;
 		return (new_node);
 	}
 	else
