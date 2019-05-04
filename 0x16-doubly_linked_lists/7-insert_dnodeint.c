f247e3f6a18e191d621b616d4fc64510d1317059
Haroldo Vélez <hvelez020@gmail.com>
2019-05-04 20:09:10 +0000

    [fix] if empty create the list

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (new_node == NULL)
 		return (NULL);
-	if (idx == 0)
+	if (idx == 0 || *h == NULL)
 	{
 		(*new_node).n = n;
 		(*new_node).next = *h;
