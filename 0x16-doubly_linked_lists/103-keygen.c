0be74628a6150921d468e9eb5044edee5988f872
Haroldo Vélez <hvelez020@gmail.com>
2019-05-07 03:58:23 +0000

    [add]task#103

--- 0x16-doubly_linked_lists/103-keygen.c
+++ 0x16-doubly_linked_lists/103-keygen.c
@@ -49,18 +49,18 @@ int get_greatest(char *str)
 }
 
 /**
- *get_f5 - get f5 operations for crackme5
+ *_strlen - calculates the length of a string
  *@str: string
  *Return: result
  */
 
-int get_f5(char *str)
+int _strlen(char *str)
 {
-	unsigned int i, buf = 0;
+	unsigned int i = 0;
 
-	for (i = 0; i < strlen(str); i++)
-		buf += (unsigned int) str[i] * (unsigned int) str[i];
-	return (buf);
+	while (str[i] != '\0')
+		i++;
+	return (i);
 }
 
 /**
@@ -73,26 +73,42 @@ int get_f5(char *str)
 int main(int argc, char *argv[])
 {
 	(void) argc;
-	unsigned int i, buf = 0;
+	unsigned int i = 0, j = 0, buf = 0;
 	char *str;
 	/*create table*/
-	char letters[] = "kuoIKdhPr1aQ3FeZqxf6Wjm4+ULYcJGOtvzX5syVMnpgbiT2B9E0SNl78wDRHC-As4E{galf";
+	char part1[] = "kuoIKdhPr1aQ3FeZqxf6Wjm4+ULYcJGOtvzX5s";
+	char part2[] = "yVMnpgbiT2B9E0SNl78wDRHC-As4E{galf";
+	char *letters = malloc(_strlen(part1) + _strlen(part2) + 1);
 	/*create password*/
 	char pass[7];
 
+	while (part1[i] != '\0' || part2[j - i] != '\0')
+	{
+		if (part1[i] != '\0')
+		{
+			letters[i] = part1[i];
+			i++;
+		}
+		else
+			letters[j] = part2[j - i];
+		j++;
+	}
+	letters[j + i] = '\0';
 	srand((get_greatest(argv[1]) ^ 0xe));
-	pass[0] = letters[0x3f - ((strlen(argv[1]) ^ 0x3b) & 0x3f)];
+	pass[0] = letters[0x3f - ((_strlen(argv[1]) ^ 0x3b) & 0x3f)];
 	pass[1] = letters[0x3f - ((get_sum(argv[1]) ^ 0x4f) & 0x3f)];
 	pass[2] = letters[0x3f - ((get_mul(argv[1]) ^ 0x55) & 0x3f)];
 	pass[3] = letters[0x3f - (rand() & 0x3f)];
-	pass[4] = letters[0x3f - ((get_f5(argv[1]) ^ 0xef) & 0x3f)];
+	buf = 0;
 	str = argv[1];
+	for (i = 0; i < strlen(str); i++)
+		buf += (unsigned int) str[i] * (unsigned int) str[i];
+	pass[4] = letters[0x3f - ((buf ^ 0xef) & 0x3f)];
+	buf = 0;
 	for (i = 0; i < (unsigned int) str[0]; i++)
 		buf = rand();
 	pass[5] = letters[0x3f - ((buf ^ 0xe5) & 0x3f)];
 	pass[6] = '\0';
 	printf("%s", pass);
-
-
 	return (1);
 }
