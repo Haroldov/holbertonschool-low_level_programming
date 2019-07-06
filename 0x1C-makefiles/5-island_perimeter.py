b8d800f20ac6ef9b75bdc5f0f48c95073d435ef5
Haroldo Vélez <hvelez020@gmail.com>
2019-07-06 03:00:40 +0000

    [fix] when vertice is 0

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,13 +5,10 @@
 def island_perimeter(grid):
     """ doc """
 
-    width = 0
-    for row in grid:
-        if sum(row) > width:
-            width = sum(row)
+    grid = list(filter(lambda x: sum(x) > 0, grid))
     grid = list(map(lambda x: list(x), zip(*grid)))
-    height = 0
-    for row in grid:
-        if sum(row) > height:
-            height = sum(row)
+    grid = list(filter(lambda x: sum(x) > 0, grid))
+    height = len(grid)
+    grid = list(map(lambda x: list(x), zip(*grid)))
+    width = len(grid)
     return (2 * height + 2 * width)
