42d6e6989b73a0cbffc49f4305c8cb2e89e2ec35
Haroldo Vélez <hvelez020@gmail.com>
2019-07-24 17:07:47 +0000

    [fix] task #0

--- 0x1D-binary_trees/102-binary_tree_is_complete.c
+++ 0x1D-binary_trees/102-binary_tree_is_complete.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "binary_trees.h"
-int leaves_are_perfect(const binary_tree_t *tree, size_t sw);
 
 /**
  *binary_tree_is_complete - checks if a binary tree is complete
@@ -16,34 +15,22 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	is_perf = binary_tree_is_perfect(tree);
-	if (is_perf == 0)
-	{
-		printf("NOT PERFECT\n");
-		return (0);
-	}
-	is_perf = leaves_are_perfect(tree, binary_tree_height(tree));
-	return (is_perf);
+	if (is_perf == 1)
+		return (1);
+	return (is_perf(tree, binary_tree_height(tree)));
 }
 
 /**
- *leaves_are_perfect - check if leaves are left justified
+ *is_perf - check perfection
  *@tree: pointer to the root node of the tree to check
- *Return: 1 if perfect
+ *@h: height of the tree
  */
-
-int leaves_are_perfect(const binary_tree_t *tree, size_t sw)
+int is_perf(const binary_tree_t *tree, size_t h)
 {
 	if (tree == NULL)
 		return (0);
-	if (tree->left == NULL && tree->right == NULL)
-	{
-		printf("%li ---- %li\n", (size_t)binary_tree_depth(tree), sw);
-		if ((size_t)binary_tree_depth(tree) == sw)
-			return (1);
-		else
-			return (0);
-	}
-	return (leaves_are_perfect(tree->left, sw) && leaves_are_perfect(tree->right, sw));
+	
+
 }
 
 /**
@@ -56,18 +43,15 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	int tmp;
 
-	if (tree == NULL || tree->left == 0)
+	if (tree == NULL)
 		return (0);
-	if (tree->left->left == NULL && tree->left->right == NULL)
-		return (1);
-	if (tree->right->right == NULL && tree->right->left == NULL)
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 	tmp = binary_tree_is_perfect(tree->left)
 		&& binary_tree_is_perfect(tree->right);
 	return (tmp && binary_tree_height(tree->left) == binary_tree_height(
 			tree->right));
 }
-
 
 /**
  *binary_tree_height - measures the height of a binary tree
