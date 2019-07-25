6ca2718f283118475993fba6ce093947a45bad47
Haroldo Vélez <hvelez020@gmail.com>
2019-07-25 01:09:14 +0000

    [add] task #26

---
+++ 0x1D-binary_trees/112-array_to_bst.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ *array_to_bst - builds a Binary Search Tree from an array
+ *@array: pointer to the first element of the array to be converted
+ *@size: number of elements in the array
+ *Return: pointer to the root node of the created BST, or NULL on failure
+ */
+
+bst_t *array_to_bst(int *array, size_t size)
+{
+	bst_t **root = NULL;
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		bst_insert(&root, array[i]);
+	}
+	return (*root);
+}
