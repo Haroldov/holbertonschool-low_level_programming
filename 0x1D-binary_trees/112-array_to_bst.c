bf990f11d4c89676afd950a70a3f31826f78a8e4
Haroldo Vélez <hvelez020@gmail.com>
2019-07-25 01:10:48 +0000

    [fix] task #25

--- 0x1D-binary_trees/112-array_to_bst.c
+++ 0x1D-binary_trees/112-array_to_bst.c
@@ -10,12 +10,12 @@
 
 bst_t *array_to_bst(int *array, size_t size)
 {
-	bst_t **root = NULL;
-	int i;
+	bst_t *root = NULL;
+	size_t i;
 
 	for (i = 0; i < size; i++)
 	{
 		bst_insert(&root, array[i]);
 	}
-	return (*root);
+	return (root);
 }
